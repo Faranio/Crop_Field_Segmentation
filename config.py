0e2515cd6e34087e2de8f23447d2d6d337e7a604
Faranio <agent006_99@mail.ru>
2021-05-21 15:21:26 +0600

    Add inference to RGB-version branch

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import lgblkb_tools
 
 settings = dynaconf.Dynaconf(settings_files=['settings.yaml'], environments=True, load_dotenv=True)
 project_folders = box.Box({k: lgblkb_tools.Folder(v, assert_exists=True) for k, v in settings.project.dirs.items()}, frozen_box=True)
+cache_folder = project_folders.cache_folder
 data_folder = project_folders.data_folder
 downloaded_folder = data_folder['Downloaded_Data']
 train_folder = data_folder['Training_Data']
