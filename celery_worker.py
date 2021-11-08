a0a64505d63c50b0a1162502da7a7931b3a353f6
Faranio <agent006_99@mail.ru>
2021-11-08 20:58:36 +0200

    Make predictions

--- celery_worker.py
+++ celery_worker.py
@@ -11,7 +11,7 @@ from inference import predict_safe_regions
 app = Celery('Crop_Field_Segmentation', broker=broker_url, backend='rpc://')
 app.config_from_object(settings.CELERY.config)
 
-folder_path = '/usr/src/app/temp'
+folder_path = '/usr/src/app/data/04_test'
 resultant_file_paths = os.listdir(folder_path)
 # resultant_file_paths = []
 
@@ -33,20 +33,20 @@ resultant_file_paths = os.listdir(folder_path)
 #             cur_file_path = os.path.join(folder_path, file)
 #             resultant_file_paths.append(cur_file_path)
             
-count = 0
-total = len(resultant_file_paths)
-
-for file_path in resultant_file_paths:
-    file_name = file_path.split('/')[-1]
-    count += 1
-    
-    num_files = len(os.listdir(data_folder['04_test'][file_name].path))
-    
-    if num_files < 4:
-        shutil.rmtree(data_folder['04_test'][file_name].path)
-        copy_tree(os.path.join(folder_path, file_path), data_folder['04_test'][file_name].path)
-
-    print(f"Copied {count} files out of {total}.")
+# count = 0
+# total = len(resultant_file_paths)
+#
+# for file_path in resultant_file_paths:
+#     file_name = file_path.split('/')[-1]
+#     count += 1
+#
+#     num_files = len(os.listdir(data_folder['04_test'][file_name].path))
+#
+#     if num_files < 4:
+#         shutil.rmtree(data_folder['04_test'][file_name].path)
+#         copy_tree(os.path.join(folder_path, file_path), data_folder['04_test'][file_name].path)
+#
+#     print(f"Copied {count} files out of {total}.")
 
 # resultant_file_paths = os.listdir(data_folder['04_test'])
 
@@ -56,16 +56,16 @@ for file_path in resultant_file_paths:
 for file_path in resultant_file_paths:
     file_name = file_path.split('/')[-1]
     
-    print(f"{file_name}: {os.listdir(os.path.join(folder_path, file_path))}")
+    # print(f"{file_name}: {os.listdir(os.path.join(folder_path, file_path))}")
 
-    # if os.path.exists(data_folder['03_results'][f'{file_name}.gpkg']):
-    #     continue
-    #
-    # predict_safe_regions(
-    #     safe_folder_path=file_path,
-    #     tile_width=20000,
-    #     tile_height=20000,
-    #     confidence=0.8,
-    #     intersection_threshold=0.5,
-    #     save=True
-    # )
+    if os.path.exists(data_folder['03_results'][f'{file_name}.gpkg']):
+        continue
+
+    predict_safe_regions(
+        safe_folder_path=file_path,
+        tile_width=20000,
+        tile_height=20000,
+        confidence=0.8,
+        intersection_threshold=0.5,
+        save=True
+    )
