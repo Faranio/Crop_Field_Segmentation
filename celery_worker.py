bdc3237681c9e51cf3a6d9c2c8b83c406cbd0beb
Faranio <agent006_99@mail.ru>
2021-11-08 22:14:43 +0200

    Debugging

--- celery_worker.py
+++ celery_worker.py
@@ -11,7 +11,7 @@ from inference import predict_safe_regions
 app = Celery('Crop_Field_Segmentation', broker=broker_url, backend='rpc://')
 app.config_from_object(settings.CELERY.config)
 
-folder_path = '/usr/src/app/temp'
+folder_path = '/usr/src/app/data'
 resultant_file_paths = os.listdir(folder_path)
 # resultant_file_paths = []
 
@@ -33,27 +33,27 @@ resultant_file_paths = os.listdir(folder_path)
 #             cur_file_path = os.path.join(folder_path, file)
 #             resultant_file_paths.append(cur_file_path)
             
-count = 0
-total = len(resultant_file_paths)
-
-for file_path in resultant_file_paths:
-    file_name = file_path.split('/')[-1]
-    files = os.listdir(os.path.join(folder_path, file_path))
-
-    num_files = len(os.listdir(data_folder['04_test'][file_name]))
-
-    if num_files < 4:
-        for file in files:
-            if os.path.exists(data_folder['04_test'][file_name][file]):
-                continue
-                
-            shutil.copyfile(os.path.join(folder_path, file_path, file), data_folder['04_test'][file_name][file])
-            
-    count += 1
-
-    print(f"Copied {count} folders out of {total}.")
-
-resultant_file_paths = os.listdir(data_folder['04_test'])
+# count = 0
+# total = len(resultant_file_paths)
+#
+# for file_path in resultant_file_paths:
+#     file_name = file_path.split('/')[-1]
+#     files = os.listdir(os.path.join(folder_path, file_path))
+#
+#     num_files = len(os.listdir(data_folder['04_test'][file_name]))
+#
+#     if num_files < 4:
+#         for file in files:
+#             if os.path.exists(data_folder['04_test'][file_name][file]):
+#                 continue
+#
+#             shutil.copyfile(os.path.join(folder_path, file_path, file), data_folder['04_test'][file_name][file])
+#
+#     count += 1
+#
+#     print(f"Copied {count} folders out of {total}.")
+#
+# resultant_file_paths = os.listdir(data_folder['04_test'])
 
 for file_path in resultant_file_paths:
     file_name = file_path.split('/')[-1]
@@ -61,8 +61,8 @@ for file_path in resultant_file_paths:
     
     # print(f"{file_name}: {os.listdir(os.path.join(folder_path, file_path))}")
     
-    if count < 4:
-        print(f"{file_name}: {os.listdir(os.path.join(folder_path, file_path))}")
+    if len(files) < 4:
+        print(f"{file_name}: {files}")
 
     # if os.path.exists(data_folder['03_results'][f'{file_name}.gpkg']):
     #     continue
