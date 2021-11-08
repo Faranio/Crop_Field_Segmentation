7e69695d26e56ca63076f04511368d613c1445a7
Faranio <agent006_99@mail.ru>
2021-11-08 17:26:47 +0200

    Make predictions

--- celery_worker.py
+++ celery_worker.py
@@ -5,63 +5,63 @@ from celery import Celery
 from distutils.dir_util import copy_tree
 
 from config import broker_url, data_folder, settings
-# from inference import predict_safe_regions
+from inference import predict_safe_regions
 
 app = Celery('Crop_Field_Segmentation', broker=broker_url, backend='rpc://')
 app.config_from_object(settings.CELERY.config)
 
-folder_path = '/usr/src/app/temp'
-safe_file_paths = os.listdir(folder_path)
-resultant_file_paths = []
+folder_path = '/usr/src/app/data/04_test'
+resultant_file_paths = os.listdir(folder_path)
+# resultant_file_paths = []
 
-for file in safe_file_paths:
-    keywords = [f'20211{x}' for x in [
-        '024',
-        '025',
-        '026',
-        '027',
-        '028',
-        '029',
-        '030',
-        '031',
-        '101'
-    ]]
-    
-    for keyword in keywords:
-        if keyword in file:
-            cur_file_path = os.path.join(folder_path, file)
-            resultant_file_paths.append(cur_file_path)
+# for file in safe_file_paths:
+#     keywords = [f'20211{x}' for x in [
+#         '024',
+#         '025',
+#         '026',
+#         '027',
+#         '028',
+#         '029',
+#         '030',
+#         '031',
+#         '101'
+#     ]]
+#
+#     for keyword in keywords:
+#         if keyword in file:
+#             cur_file_path = os.path.join(folder_path, file)
+#             resultant_file_paths.append(cur_file_path)
             
-count = 0
-total = len(resultant_file_paths)
-            
-for file_path in resultant_file_paths:
-    file_name = file_path.split('/')[-1]
-    count += 1
-    
-    try:
-        copy_tree(file_path, data_folder['04_test'][file_name].path)
-    except FileExistsError:
-        continue
-    
-    print(f"Copied {count} files out of {total}.")
-    
-resultant_file_paths = os.listdir(data_folder['04_test'])
-
-print(f"safe_file_paths: {safe_file_paths}")
-print(f"resultant_file_paths: {resultant_file_paths}")
-
+# count = 0
+# total = len(resultant_file_paths)
+#
 # for file_path in resultant_file_paths:
 #     file_name = file_path.split('/')[-1]
+#     count += 1
 #
-#     if os.path.exists(data_folder['03_results'][f'{file_name}.gpkg']):
+#     try:
+#         copy_tree(file_path, data_folder['04_test'][file_name].path)
+#     except FileExistsError:
 #         continue
 #
-#     predict_safe_regions(
-#         safe_folder_path=file_path,
-#         tile_width=20000,
-#         tile_height=20000,
-#         confidence=0.8,
-#         intersection_threshold=0.5,
-#         save=True
-#     )
+#     print(f"Copied {count} files out of {total}.")
+#
+# resultant_file_paths = os.listdir(data_folder['04_test'])
+#
+# print(f"safe_file_paths: {safe_file_paths}")
+# print(f"resultant_file_paths: {resultant_file_paths}")
+
+for file_path in resultant_file_paths:
+    file_name = file_path.split('/')[-1]
+
+    if os.path.exists(data_folder['03_results'][f'{file_name}.gpkg']):
+        continue
+
+    predict_safe_regions(
+        safe_folder_path=file_path,
+        tile_width=20000,
+        tile_height=20000,
+        confidence=0.8,
+        intersection_threshold=0.5,
+        save=True
+    )
