19f4fa644c77e0d94392cff8023339b38bbbc773
Faranio <agent006_99@mail.ru>
2021-11-08 11:51:19 +0200

    Remove copying images

--- celery_worker.py
+++ celery_worker.py
@@ -45,8 +45,8 @@ for file in safe_file_paths:
             cur_file_path = os.path.join(folder_path, file)
             resultant_file_paths.append(cur_file_path)
             
-for file_path in resultant_file_paths:
-    shutil.copytree(file_path, data_folder['05_data'])
+# for file_path in resultant_file_paths:
+#     shutil.copytree(file_path, data_folder['05_data'])
 
 for file_path in resultant_file_paths:
     file_name = file_path.split('/')[-1]
